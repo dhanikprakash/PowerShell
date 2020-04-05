aa7eb1cc136c84d0a587d556d5a2c4c05e19c0f5
dhanikprakash <dhanikprakash@gmail.com>
2020-04-05 10:51:06 +0100

    init

---
+++ powershell_system.ps1
@@ -0,0 +1,22 @@
+﻿#get a breaddown of error sources in the system eventlog
+
+Param(
+[string]$Computername = $env:COMPUTERNAME,
+[string]$ReportTitle = "Event Log Report", 
+[Parameter(Mandatory, HelpMessage ="Enter the path for the file")]
+[string]$path
+
+)
+$computername = $env:COMPUTERNAME
+$data = Get-Process | where Cpu -GT 100
+
+
+$footer = "<h5> Report Run $(get-date)</h5>"
+$css = "http://jdhitsolutions.com/sample.css"
+
+$data | sort -Property Count, Name -Descending |
+select CPU, Name | 
+ConvertTo-Html -Title $ReportTitle -PreContent "<h1>$computername</h1>" -PostContent $footer  -CssUri $css |
+Out-File $path
+
+
